dec516adc39d53dc82212b08ebb8bf6bed1ddf90
Kuras <lukevito@gmail.com>
2018-12-08 00:10:03 +0100

    sad

--- src/main/java/com/gmail/com/fantasy_api/CircularDependencyAppContextAwareAndInitializingBeanA.java
+++ src/main/java/com/gmail/com/fantasy_api/CircularDependencyAppContextAwareAndInitializingBeanA.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Component;
 //https://stackoverflow.com/questions/45210671/whats-is-the-order-dependency-injection-in-spring?noredirect=1&lq=1
 //TODO: https://www.baeldung.com/spring-depends-on
 //https://www.baeldung.com/circular-dependencies-in-spring
+//TODO:https://www.baeldung.com/spring-autowire
+//TODO:https://docs.spring.io/spring/docs/1.2.x/reference/beans.html#beans-factory-lifecycle
 @Component
 public class CircularDependencyAppContextAwareAndInitializingBeanA implements ApplicationContextAware, InitializingBean {
 
